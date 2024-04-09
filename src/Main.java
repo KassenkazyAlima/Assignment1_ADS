efaf2a26c2476b65ccd552db53045fe35248ad28
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-04-09 18:17:23 +0500

    Task 2 with its explanation (comments) and I also added the question to the main method where switches the cases so it will be clear for users what task is being solved.

--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,43 @@ public class Main {
 
         switch (taskNum) {
             case 1:
+                System.out.println("User should input a number “n” and an array of “n” elements, and the function returns minimum of them.");
                 task1();
                 break;
             case 2:
+                System.out.println("User should input a number “n” and an array of “n” elements, and the function calculates the average of the numbers.");
                 task2();
                 break;
             case 3:
+                System.out.println("User should input a number “n”, and the function checks whether it is prime or not.");
                 task3();
                 break;
             case 4:
+                System.out.println("User should input a number “n”, and the function returns factorial of it.");
                 task4();
                 break;
             case 5:
+                System.out.println("User should input a number “n”, and the function returns n-th element in Fibonacci sequence using recursion.");
                 task5();
                 break;
             case 6:
+                System.out.println("User should input numbers “n” and “a”, and the function returns a to power of n.");
                 task6();
                 break;
             case 7:
+                System.out.println("User should input numbers “n” and an array of “n” elements, and the function returns the given array in reverse order without using array data structure.");
                 task7();
                 break;
             case 8:
+                System.out.println("User should input a string “s”, and the function checks whether s is all consist of digits.");
                 task8();
                 break;
             case 9:
+                System.out.println("User should input numbers “n” and “k”, and the function calculates the binomial coefficient using the particular formula.");
                 task9();
                 break;
             case 10:
+                System.out.println("User should input numbers “a” and “b”, and the function returns the GCD(a,b).");
                 task10();
                 break;
             default:
@@ -45,7 +55,14 @@ public class Main {
 
         }
     }
-
+    /*
+    Problem 1
+    This method finds the minimum element of an array by iterating through the array that user will input.
+    Time complexity: O(n), where n is the input number.
+    Result in linear time complexity
+    @param- n, the size of elements in the array
+    @return - minimum num of elements
+     */
     public static void task1() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array: ");
@@ -59,7 +76,7 @@ public class Main {
         int min = arr[0];
         min = findMin(arr, n, min);
         System.out.println("Minimum element is: " + findMin(arr, n, min));
-
+        System.out.println("Time taken: ");
     }
     private static int findMin(int[] arr, int n, int min) {
         for (int i = 1; i < n; i++) {
@@ -69,7 +86,14 @@ public class Main {
         }
         return min;
     }
-
+    /*
+    Problem 2
+    This method calculates the average number of elements in an array by finding the sum and divide to number of elements.
+    Time complexity: O(n), where n is the input number.
+    Result in linear time complexity
+    @param- n, the size of elements in the array
+    @return - minimum num of elements
+     */
     public static void task2() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array: ");
@@ -170,13 +194,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String s = sc.next();
-//        if(isAllDigits(s))
-//            System.out.println("yes");
-//        else
-//            System.out.println("no");
-//    }
-//    private static boolean isAllDigits(String s){
-//
+        if(isAllDigits(s))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+    private static boolean isAllDigits(String s){
+        if(s.isEmpty())
+            return true;
+        if(!Character.isDigit(s.charAt(0)))
+            return false;
+        return isAllDigits(s.substring(1));
+
     }
     public static void task9() {
         Scanner sc = new Scanner(System.in);
