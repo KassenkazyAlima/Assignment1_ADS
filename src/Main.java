e53dcdfbb05e177cbfc140083eebae3744a199ee
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-04-09 23:14:42 +0500

    tasks w

--- src/Main.java
+++ src/Main.java
@@ -152,7 +152,7 @@ public class Main {
 
     private static boolean isPrime(int n, int i){
         if(n<=2)
-            return (n==2) ? true : false;
+            return n==2 ? true : false;
         if(n%i == 0)
             return false;
         if(i*i > n)
